18ec5402cb3fe3f97c9621ba685b737266389e6d
Peng <133524637+PengeSal@users.noreply.github.com>
2023-12-27 21:04:30 +0000

    Update imageserver.py

--- LAN Messager/imageserver.py
+++ LAN Messager/imageserver.py
@@ -48,18 +48,21 @@ def handle_client(client_socket):
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-hostname = socket.gethostname()
-server_address = (hostname, 5556)
-server_socket.bind(server_address)
 
-server_socket.listen(1)
-print("Server is listening for connections...")
+try:
+    hostname = socket.gethostname()
+    server_address = (hostname, 5556)
+    server_socket.bind(server_address)
 
-while True:
-    print("Waiting for a connection...")
-    client_socket, client_address = server_socket.accept()
-    print(f"Connection from {client_address}")
+    server_socket.listen(1)
+    print("Server is listening for connections...")
 
-    client_handler = threading.Thread(target=handle_client, args=(client_socket,))
-    client_handler.start()
+    while True:
+        print("Waiting for a connection...")
+        client_socket, client_address = server_socket.accept()
+        print(f"Connection from {client_address}")
 
+        client_handler = threading.Thread(target=handle_client, args=(client_socket,))
+        client_handler.start()
+except OSError:
+    pass
